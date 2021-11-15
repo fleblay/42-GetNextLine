3ae11ac689edf678087761f815324bc0caf6f07d
Fred <frederic.leblay@gmail.com>
2021-11-15 19:18:42 +0100

    Working on GNL

--- get_next_line.c
+++ get_next_line.c
@@ -6,12 +6,13 @@
 /*   By: fle-blay <fle-blay@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 11:01:05 by fle-blay          #+#    #+#             */
-/*   Updated: 2021/11/15 11:58:36 by fle-blay         ###   ########.fr       */
+/*   Updated: 2021/11/15 19:16:40 by fle-blay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <unistd.h>
+#include <stdio.h>
 
 void	ft_bzero(void *s, unsigned int n)
 {
@@ -34,10 +35,16 @@ static char	*load_content(int fd, char **dest)
 	ft_bzero(buf, BUFFER_SIZE);
 	cntsize = read(fd, buf, BUFFER_SIZE);
 	if (cntsize <= 0)
+	{
+		free(*dest);
 		return (NULL);
+	}
 	content = ft_strrawjoin(*dest, buf, cntsize);
 	if (! content)
+	{	
+		free(*dest);
 		return (NULL);
+	}
 	free(*dest);
 	*dest = NULL;
 	return (content);
@@ -49,16 +56,29 @@ char	*get_next_line(int fd)
 	char		*tmp = NULL;
 	char		*togive = NULL;
 
-	mainstr = (char *) malloc(sizeof(char));
+	if (! mainstr)
+		mainstr = ft_strdup("\0");
 	if (! mainstr)
 		return (NULL);
-	mainstr[0] = '\0';
-	while (! ft_strchr(mainstr, '\n'))
+	//printf("ici\n");
+	while (mainstr && ! ft_strchr(mainstr, '\n'))
+	{
+
+		//printf("toto\n");
 		mainstr = load_content(fd, &mainstr);
-	togive = ft_substr(mainstr, 0, ft_strchr(mainstr, '\n') - mainstr + 2);
+		//printf(" main str pdt chargement : '%s'\n", mainstr);
+		//printf("tata\n");
+	}
+	if (! mainstr)
+		return (NULL);
+	//printf("la\n");
+	togive = ft_substr(mainstr, 0, ft_strchr(mainstr, '\n') - mainstr + 1);
+	//printf("to give : '%s'\n", togive);
 	tmp = ft_strdup(mainstr);
+	//printf(" tmp = mainstr : '%s'\n", tmp);
 	free(mainstr);
 	mainstr = ft_substr(tmp, ft_strchr(tmp, '\n') - tmp + 1, ft_strlen(tmp));
+	//printf(" main str restant : '%s'\n", mainstr);
 	free(tmp);
 	return (togive);
 }
@@ -74,17 +94,13 @@ int main (void)
 
 	fd = open("./test.txt", O_RDONLY);
 	test = NULL;
-	test = (char *)malloc(1 * sizeof(char));
-	test[0] = '\0';
 
-	test = get_next_line(fd);
-	printf("test : %s\n", test);
+	while ((test = get_next_line(fd)) != NULL)
+	{
+		printf("test : %s", test);
+		free (test);
+		test = NULL;
+	}
 
-	test = get_next_line(fd);
-	printf("test : %s\n", test);
-
-	free (test);
-	test = NULL;
-	
 	return (0);
 }
