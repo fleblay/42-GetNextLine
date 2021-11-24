10b3fefe6a7feaff284fa49b512edea24b5171c8
Frederic LE BLAY <fle-blay@e1r1p10.42.fr>
2021-11-24 16:48:01 +0100

    Fix memchr to comply with libft-unit-tester

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -1,18 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line_opti_bonus.c                         :+:      :+:    :+:   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fle-blay <fle-blay@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fle-blay <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/11 11:01:05 by fle-blay          #+#    #+#             */
-/*   Updated: 2021/11/18 15:38:05 by fle-blay         ###   ########.fr       */
+/*   Created: 2021/11/24 16:20:03 by fle-blay          #+#    #+#             */
+/*   Updated: 2021/11/24 16:23:52 by fle-blay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line_bonus.h"
 #include <unistd.h>
-#include <stdio.h>
 
 int	load_content(int fd, char **dest)
 {
@@ -25,27 +24,67 @@ int	load_content(int fd, char **dest)
 	if (cntsize <= 0)
 		return (cntsize);
 	content = ft_strrawjoin(*dest, buf, cntsize);
+	if (! content)
+	{
+		multi_freenull(dest, NULL, NULL, NULL);
+		return (-2);
+	}
 	free(*dest);
 	*dest = content;
 	return (cntsize);
 }
 
-int	update_str(char **togive, char **mainstr)
+void	update_str(char **togive, char **mainstr)
 {
 	char	*tmp;
 
-	tmp = NULL;
 	*togive = ft_substr(*mainstr, 0, ft_strchr(*mainstr, '\n') - *mainstr + 1);
-	tmp = ft_strdup(*mainstr);
-	free(*mainstr);
-	*mainstr = ft_substr(tmp, ft_strchr(tmp, '\n') - tmp + 1, ft_strlen(tmp));
-	if (! ft_strlen(*mainstr))
+	if (! *togive)
 	{
 		free(*mainstr);
-		*mainstr = NULL;
+		*togive = NULL;
+		return ;
 	}
+	tmp = ft_strdup(*mainstr);
+	if (! tmp)
+	{
+		multi_freenull(mainstr, togive, NULL, NULL);
+		return ;
+	}
+	free(*mainstr);
+	*mainstr = ft_substr(tmp, ft_strchr(tmp, '\n') - tmp + 1, ft_strlen(tmp));
+	if (! *mainstr)
+	{
+		multi_freenull(&tmp, mainstr, togive, NULL);
+		return ;
+	}
+	if (! ft_strlen(*mainstr))
+		multi_freenull(mainstr, NULL, NULL, NULL);
 	free(tmp);
-	return (1);
+}
+
+void	multi_freenull(char **s1, char **s2, char **s3, char **s4)
+{
+	if (s1 && *s1)
+	{
+		free(*s1);
+		*s1 = NULL;
+	}
+	if (s2 && *s2)
+	{
+		free(*s2);
+		*s2 = NULL;
+	}
+	if (s3 && *s3)
+	{
+		free(*s3);
+		*s3 = NULL;
+	}
+	if (s4 && *s4)
+	{
+		free(*s4);
+		*s4 = NULL;
+	}
 }
 
 char	*get_next_line(int fd)
@@ -64,6 +103,8 @@ char	*get_next_line(int fd)
 		read = load_content(fd, &mainstr[fd]);
 		if (read <= 0)
 		{
+			if (read == -2)
+				return (NULL);
 			togive = ft_strdup(mainstr[fd]);
 			free(mainstr[fd]);
 			mainstr[fd] = NULL;
@@ -74,25 +115,3 @@ char	*get_next_line(int fd)
 	update_str(&togive, &mainstr[fd]);
 	return (togive);
 }
-
-/*#include <fcntl.h>
-#include <stdio.h>
-
-int	main(int ac, char *av[])
-{
-	char	*test;
-	int		fd;
-
-	test = NULL;
-	if (ac == 2)
-		fd = open(av[1], O_RDONLY);
-	else
-		fd = 0;
-	while ((test = get_next_line(fd)) != NULL)
-	{
-		printf("\x1b[31mtest : '%s'\x1b[0m", test);
-		free (test);
-		test = NULL;
-	}
-	return (0);
-}*/
--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,7 +6,7 @@
 /*   By: fle-blay <fle-blay@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 13:54:25 by fle-blay          #+#    #+#             */
-/*   Updated: 2021/11/18 14:24:32 by fle-blay         ###   ########.fr       */
+/*   Updated: 2021/11/24 16:38:05 by fle-blay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,8 @@ char			*ft_strrawjoin(char const *s1, char const *s2, int s2size);
 char			*ft_substr(char const *s, unsigned int start, unsigned int len);
 void			ft_bzero(void *s, unsigned int n);
 int				load_content(int fd, char **dest);
-int				update_str(char **togive, char **mainstr);
+void			update_str(char **togive, char **mainstr);
+void			multi_freenull(char **s1, char **s2, char **s3, char **s4);
 char			*get_next_line(int fd);
 
 #endif
